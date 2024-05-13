ef671b5f6b5eeffe70b93d75701b36f0d5a6d3cb
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-05-13 12:28:20 +0900

    hello world

---
+++ hello_world/src/main.rs
@@ -0,0 +1,4 @@
+// ビルドするためにgccのインストールが必要
+fn main() {
+    println!("Hello, world!");
+}
---
+++ hello_world/target/debug/deps/hello_world-4e828ec85f6c1b1c.d
@@ -0,0 +1,5 @@
+/home/r-horie/workspaces/rust-intro/hello_world/target/debug/deps/libhello_world-4e828ec85f6c1b1c.rmeta: src/main.rs
+
+/home/r-horie/workspaces/rust-intro/hello_world/target/debug/deps/hello_world-4e828ec85f6c1b1c.d: src/main.rs
+
+src/main.rs:
