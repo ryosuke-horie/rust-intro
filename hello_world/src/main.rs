564eabbd1dab9d4a0d20b24f811fd46de1c05382
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-05-13 12:31:48 +0900

    printマクロ

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,12 @@
 // ビルドするためにgccのインストールが必要
 fn main() {
+    // printlnは最後に開業を含める
     println!("Hello, world!");
+
+    // print!は最後に改行を含めない
+    print!("こ");
+    print!("ん");
+    print!("に");
+    print!("ち");
+    print!("は");
 }
---
+++ hello_world/target/debug/deps/hello_world-4c0c3ec61a897073.d
@@ -0,0 +1,5 @@
+/home/r-horie/workspaces/rust-intro/hello_world/target/debug/deps/libhello_world-4c0c3ec61a897073.rmeta: src/main.rs
+
+/home/r-horie/workspaces/rust-intro/hello_world/target/debug/deps/hello_world-4c0c3ec61a897073.d: src/main.rs
+
+src/main.rs:
