0def5b45c835d7c018a617d62e2ec17e326d624d
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-05-13 12:32:35 +0900

    build成果物を除外

--- hello_world/target/debug/deps/hello_world-495e70b5bc18f55c.d
+++
@@ -1,5 +0,0 @@
-/home/r-horie/workspaces/rust-intro/hello_world/target/debug/deps/hello_world-495e70b5bc18f55c: src/main.rs
-
-/home/r-horie/workspaces/rust-intro/hello_world/target/debug/deps/hello_world-495e70b5bc18f55c.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/r-horie/workspaces/rust-intro/hello_world/target/debug/hello_world: /home/r-horie/workspaces/rust-intro/hello_world/src/main.rs
